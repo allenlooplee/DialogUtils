b8b8278a41551f11a668418f13eab21a990ecba5
Allen Lee <looplee@msn.com>
2021-05-09 20:48:38 +0800

    Resolve resource issues.

---
+++ DialogUtils/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DialogUtils.Converters
+{
+    class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool flag = false;
+            if (value is bool)
+            {
+                flag = (bool)value;
+            }
+            else if (value is bool?)
+            {
+                bool? nullable = (bool?)value;
+                flag = nullable.HasValue ? nullable.Value : false;
+            }
+
+            bool inverse = (parameter as string) == "inverse";
+
+            if (inverse)
+            {
+                return (flag ? Visibility.Collapsed : Visibility.Visible);
+            }
+            else
+            {
+                return (flag ? Visibility.Visible : Visibility.Collapsed);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ DialogUtils/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DialogUtils.Converters
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var flag = value == null;
+            var inverse = (parameter as string) == "inverse";
+
+            if (inverse)
+            {
+                return (flag ? Visibility.Collapsed : Visibility.Visible);
+            }
+            else
+            {
+                return (flag ? Visibility.Visible : Visibility.Collapsed);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
