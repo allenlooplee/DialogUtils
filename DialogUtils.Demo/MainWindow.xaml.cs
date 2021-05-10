677a560189f86e65f9fa6bf191347e7d75c61f52
Allen Lee <looplee@msn.com>
2021-05-10 21:51:32 +0800

    Use MaterialWindow and AppBar.

--- DialogUtils.Demo/MainWindow.xaml.cs
+++ DialogUtils.Demo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignExtensions.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace DialogUtils.Demo
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MaterialWindow
     {
         public MainWindow()
         {
