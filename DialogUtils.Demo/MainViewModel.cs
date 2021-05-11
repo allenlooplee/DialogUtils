c6577b45469906575162cf9cecc54983adf868bc
Allen Lee <looplee@msn.com>
2021-05-11 15:29:38 +0800

    Add properties for header/message/result for show message/input scenarios.

--- DialogUtils.Demo/MainViewModel.cs
+++ DialogUtils.Demo/MainViewModel.cs
@@ -1,32 +1,63 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
+using System;
 using System.Windows.Input;
 
 namespace DialogUtils.Demo
 {
     public class MainViewModel : ObservableObject
     {
+        private const string DialogHostIdentifier = "MainHost";
         private IDialogHostService _dialogHostService;
 
-        private ICommand _showMessageCommand;
-        public ICommand ShowMessageCommand => _showMessageCommand ?? (_showMessageCommand = new RelayCommand(ShowMessageImpl));
-        private async void ShowMessageImpl()
+        private string _header;
+        public string Header
         {
-            await _dialogHostService.ShowMessageAsync("MainHost", "显示消息对话框", header: "提示", isCancelButtonVisible: true);
+            get => _header;
+            set => SetProperty(ref _header, value);
         }
 
-        private ICommand _showInputCommand;
-        public ICommand ShowInputCommand => _showInputCommand ?? (_showInputCommand = new RelayCommand(ShowInputImpl));
+        private string _message;
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                if (SetProperty(ref _message, value))
+                {
+                    ShowMessageCommand.NotifyCanExecuteChanged();
+                    ShowInputCommand.NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+        private string _result;
+        public string Result
+        {
+            get => _result;
+            set => SetProperty(ref _result, value);
+        }
+
+        private RelayCommand _showMessageCommand;
+        public RelayCommand ShowMessageCommand => _showMessageCommand ?? (_showMessageCommand = new RelayCommand(ShowMessageImpl, () => !string.IsNullOrEmpty(Message)));
+        private async void ShowMessageImpl()
+        {
+            var result = await _dialogHostService.ShowMessageAsync(DialogHostIdentifier, Message, Header, isCancelButtonVisible: true);
+            Result = result ? "Submitted" : "Cancelled";
+        }
+
+        private RelayCommand _showInputCommand;
+        public RelayCommand ShowInputCommand => _showInputCommand ?? (_showInputCommand = new RelayCommand(ShowInputImpl, () => !string.IsNullOrEmpty(Message)));
         private async void ShowInputImpl()
         {
-            await _dialogHostService.ShowInputAsync("MainHost", "显示输入对话框");
+            Result = await _dialogHostService.ShowInputAsync(DialogHostIdentifier, Message, null, Header);
         }
 
         private ICommand _showProgressCommand;
         public ICommand ShowProgressCommand => _showProgressCommand ?? (_showProgressCommand = new RelayCommand(ShowProgressImpl));
         private void ShowProgressImpl()
         {
-            _dialogHostService.ShowProgressAsync("MainHost", true, true);
+            _dialogHostService.ShowProgressAsync(DialogHostIdentifier, true, true);
         }
 
         private ICommand _showCustomCommand;
@@ -34,6 +65,15 @@ namespace DialogUtils.Demo
         private void ShowCustomImpl()
         {
 
+        }
+
+        private ICommand _clearCommand;
+        public ICommand ClearCommand => _clearCommand ?? (_clearCommand = new RelayCommand(ClearImpl));
+        private void ClearImpl()
+        {
+            Header = null;
+            Message = null;
+            Result = null;
         }
 
         public MainViewModel(IDialogHostService dialogHostService)
