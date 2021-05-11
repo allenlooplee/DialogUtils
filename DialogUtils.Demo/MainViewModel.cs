9f7c2dc87f06a82487f5f4a69a5104311648b1cd
Allen Lee <looplee@msn.com>
2021-05-11 11:19:42 +0800

    Decom Styles\Default.xaml. Set margins and font size/weight inline according to Material.io. Add two more buttons for showing input and progress dialogs.

--- DialogUtils.Demo/MainViewModel.cs
+++ DialogUtils.Demo/MainViewModel.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace DialogUtils.Demo
@@ -17,7 +12,21 @@ namespace DialogUtils.Demo
         public ICommand ShowMessageCommand => _showMessageCommand ?? (_showMessageCommand = new RelayCommand(ShowMessageImpl));
         private async void ShowMessageImpl()
         {
-            await _dialogHostService.ShowMessageAsync("MainHost", "Showing message dialog.");
+            await _dialogHostService.ShowMessageAsync("MainHost", "显示消息对话框", header: "提示", isCancelButtonVisible: true);
+        }
+
+        private ICommand _showInputCommand;
+        public ICommand ShowInputCommand => _showInputCommand ?? (_showInputCommand = new RelayCommand(ShowInputImpl));
+        private async void ShowInputImpl()
+        {
+            await _dialogHostService.ShowInputAsync("MainHost", "显示输入对话框");
+        }
+
+        private ICommand _showProgressCommand;
+        public ICommand ShowProgressCommand => _showProgressCommand ?? (_showProgressCommand = new RelayCommand(ShowProgressImpl));
+        private void ShowProgressImpl()
+        {
+            _dialogHostService.ShowProgressAsync("MainHost", true, true);
         }
 
         public MainViewModel(IDialogHostService dialogHostService)
